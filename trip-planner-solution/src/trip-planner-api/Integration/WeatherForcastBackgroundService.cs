06dcacf5971aed59bbde330fca3aa25b890657e7
Johann H. Armenteros <johann.armenteros@externals.hilti.com>
2022-12-21 16:56:59 -0600

    Created the weather background service

---
+++ trip-planner-solution/src/trip-planner-api/Integration/WeatherForcastBackgroundService.cs
@@ -0,0 +1,13 @@
+﻿namespace TripPlannerApi.Integration
+{
+    /// <summary>
+    /// Background service to retrieve the weather information and save it into the database.
+    /// </summary>
+    public class WeatherForcastBackgroundService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
