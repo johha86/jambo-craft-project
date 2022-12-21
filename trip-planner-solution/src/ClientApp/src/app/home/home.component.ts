c24b829ba3dd7d789841c810ce877437ec5b739c
Johann H. Armenteros <johann.armenteros@externals.hilti.com>
2022-12-20 23:57:42 -0600

    Added ng-bootstrap to client app

--- trip-planner-solution/src/ClientApp/src/app/home/home.component.ts
+++ trip-planner-solution/src/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public forecasts: CityInfoDto[] = [];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<CityInfoDto[]>('https://trip-planner-api20221220213642.azurewebsites.net/' + 'foo').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
+
+interface CityInfoDto {
+  name: string;
+  shortDescription: string;
+  currentWeather: string;
+  currentWeekWeather: string;
 }
